fa45fb9d8d20aaaa286299595337ae1b5e145fec
Simpl143 <86554143+Simpl143@users.noreply.github.com>
2024-03-16 15:20:31 +0300

    Initial commit

---
+++ GamingPlatformNRI-master/src/main/java/ru/example/gamingplatformnri/GamingPlatformNriApplication.java
@@ -0,0 +1,13 @@
+package ru.example.gamingplatformnri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingPlatformNriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingPlatformNriApplication.class, args);
+    }
+
+}
---
+++ GamingPlatformNRI-master/src/test/java/ru/example/gamingplatformnri/GamingPlatformNriApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.gamingplatformnri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingPlatformNriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
